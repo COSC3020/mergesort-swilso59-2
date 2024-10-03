fc475bfb8cce4d1c0c7ea5d64ffa764717f88df7
Sean W. <104464288+swilso59@users.noreply.github.com>
2024-10-03 13:47:07 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,52 @@
 function mergesort(array) {
+    // Get the length of the array 
+    const arrLength = array.length:
+
+    // Loop for subarrays
+    // There size starting at one then doubling till we reach the arrLength
+    for (let subSize = 1; subSize < arrLength; subSize *= 2) {
+        // Now we want to pick the start point or left Index of each subarray that will me merged in each current iteration 
+        for (let leftIndex = 0; leftIndex < arrLength; leftindex += 2 * subSize) {
+            // Calculate the mid point of the subarray for the merge step.
+            const middle = Math.min(leftIndex + subSize - 1, arrLength - 1);
+            console.log(mid);
+            // Calculate the ending index for the second subarray
+            const rightIndex = Math.min(leftIndex + 2 * subSize -1, arrLength - 1);
+            console.log(rightIndex);
+            // Call the merge function 
+            merge(array, leftIndex, mid, rightIndex);
+        }
+    }
+
+    function merge (array, left, mid, right) {
+    
+        // i is the starting index for the left subarray
+        let i = left;
+        // j is the starting index for the right subarray 
+        let j = mid + 1;
+
+        // merge while subarrays remain
+        while  (i <= mid && j <= right) {
+            // if the elements in the left and right subarrays are equal iterate i
+            if (array[i] <= array[j]) {
+                i++;
+            } else {
+                // if the elements in the right subarray is smaller then the element in the left subarray we need to swap elements
+                // Save the element in the right subarray
+                const temp = array[j]
+                let k = j;
+                // Shift all elements in the left subarray to the right inorder to create space for the temp element 
+                while (k !== i) {
+                    array[k] = array[k - 1];
+                    k--;
+                }
+                // Place the smaller element in the correct position 
+                array[i] = temp;
+                i++;
+                mid++;
+                j++;
+            }
+        }  
+    }
     return array;
 }
