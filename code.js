a335d2a6d95d340724fade31524f8bea33cc6fed
Sean W. <104464288+swilso59@users.noreply.github.com>
2024-10-06 13:59:34 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,50 +1,49 @@
-function mergesort(array) {
-    // Get the length of the array 
-    const n = array.length;
+function mergesort(x) {
+    let n = x.length;
 
-    // Loop for subarrays
-    // There size starting at one then doubling till we reach the arrLength
-    for (let subSize = 1; subSize < n; subSize *= 2) {
-        // Now we want to pick the start point or left Index of each subarray that will me merged in each current iteration 
-        for (let leftIndex = 0; leftIndex < n; leftIndex += 2 * subSize) {
-            // Calculate the mid point of the subarray for the merge step.
-            const mid = Math.min(leftIndex + subSize - 1, n - 1);
-            // Calculate the ending index for the second subarray
-            const rightIndex = Math.min(leftIndex + 2 * subSize - 1, n - 1);
-            // Call the merge function 
-            merge(array, leftIndex, mid, rightIndex);
+    // Current size of subarrays to merge, starting with 1
+    for (let currSize = 1; currSize < n; currSize = 2 * currSize) {
+        // Pick starting point of different subarrays of current size
+        for (let lo = 0; lo < n - currSize; lo += 2 * currSize) {
+            let mid = lo + currSize - 1;
+            let hi = Math.min(lo + 2 * currSize - 1, n - 1);
+
+            merge(x, lo, mid, hi);
         }
     }
+    return x;
+}
 
-    function merge (array, left, mid, right) {
-    
-        // i is the starting index for the left subarray
-        let i = left;
-        // j is the starting index for the right subarray 
-        let j = mid + 1;
+function merge(x, lo, mid, hi) {
+    let start2 = mid + 1;
 
-        // merge while subarrays remain
-        while  (i <= mid && j <= right) {
-            // if the elements in the left and right subarrays are equal iterate i
-            if (array[i] <= array[j]) {
-                i++;
-            } else {
-                // if the elements in the right subarray is smaller then the element in the left subarray we need to swap elements
-                // Save the element in the right subarray
-                const temp = array[j]
-                let k = j;
-                // Shift all elements in the left subarray to the right inorder to create space for the temp element 
-                while (k !== i) {
-                    array[k] = array[k - 1];
-                    k--;
-                }
-                // Place the smaller element in the correct position 
-                array[i] = temp;
-                i++;
-                mid++;
-                j++;
-            }
-        }  
+    // If the direct merge is already sorted
+    if (x[mid] <= x[start2]) {
+        return; 
     }
-    return array;
+
+    // Two pointers to maintain the current position of both subarrays
+    while (lo <= mid && start2 <= hi) {
+
+        // If the element at the start of the first half is smaller or equal, it's in the right place
+        if (x[lo] <= x[start2]) {
+            lo++;
+        } else {
+            // Otherwise, the element from the second half needs to be inserted into the first half
+            let value = x[start2];
+            let index = start2;
+
+            // Shift all elements between lo and start2 right by one position
+            while (index !== lo) {
+                x[index] = x[index - 1];
+                index--;
+            }
+            x[lo] = value;
+
+            // Update all pointers
+            lo++;
+            mid++;
+            start2++;
+        }
+    }
 }
